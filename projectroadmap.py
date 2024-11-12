e42f7155d4cb5334ecaaaf0c751b17089373ffc2
Shark1346 <agaldam3@students.kennesaw.edu>
2024-11-11 21:39:31 -0500

    Create projectroadmap.py

---
+++ projectroadmap.py
@@ -0,0 +1,25 @@
+# Project Sprints
+
+## Sprint 1  
+- [x] Submit the initial project proposal
+- [x] Set up the project repository for Tetris
+- [x] Review existing project files and tutorials for reference
+- [x] Test existing code to assess functionality and identify possible improvements
+- [x] Add the README file to the repository
+- [x] Add the project roadmap documentation to the repository
+- [x] Finish the Sprint 1 quiz
+
+## Sprint 2
+- [ ] Implement code updates with detailed commit messages
+- [ ] Integrate parts of pre-existing code as needed
+- [ ] Enhance the README file with an expanded project description
+- [ ] Adjust the visual aspects of the game for a refined look
+- [ ] Make at least serveral modifications and commit each change
+- [ ] Improve game fluidity for smoother gameplay experience
+- [ ] Complete the Sprint 2 quiz
+
+## Sprint 3
+- [ ]Prepare a PowerPoint presentation summarizing the project
+- [ ] Include essential details to communicate project goals and progress
+- [ ] Review the final code, ensuring that all elements are addressed in the presentation
+- [ ] Complete the Sprint 3 assignment
